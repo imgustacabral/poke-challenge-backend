11e10701b2fbe331e5227427eed2712a79813429
imgustacabral <imgustacabral@gmail.com>
2023-05-17 15:42:45 -0300

    docs: add api tag

--- src/infra/http/controllers/find-pokemon-abilities.controller.ts
+++ src/infra/http/controllers/find-pokemon-abilities.controller.ts
@@ -1,11 +1,13 @@
 import { Controller, Get, Param } from '@nestjs/common';
 import { FindPokemon } from '../../../application/use-cases/find-pokemon-abilities';
 import { PokemonViewModel } from '../view-model/pokemon-view-model';
+import { ApiTags } from '@nestjs/swagger';
 
-@Controller('/')
+@Controller('/pokemon')
 export class FindPokemonController {
   constructor(private findPokemon: FindPokemon) {}
 
+  @ApiTags('pokemon')
   @Get(':name')
   async find(@Param('name') name: string) {
     const pokemon = await this.findPokemon.execute({
