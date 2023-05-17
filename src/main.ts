d6868ed7ba3df0934120d13415f0b8d8d0e61224
imgustacabral <imgustacabral@gmail.com>
2023-05-17 15:41:48 -0300

    feat: add swagger config

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
+
+  const config = new DocumentBuilder()
+    .setTitle('PokeAPI Challenge')
+    .setDescription('This is an api that returns a pokemon name and abilities')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   await app.listen(3000);
 }
 bootstrap();
